2a51ed1be6463da3a35108f7c148ab2f0d0d5121
varsha1150 <167232484+varsha1150@users.noreply.github.com>
2026-05-29 15:43:48 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxsum=0;
+        for (int i=0;i<prices.length;i++){
+            for(int j=i+1;j<prices.length;j++){
+            int cursum=prices[j]-prices[i];
+            if (cursum>maxsum) maxsum=cursum;
+
+        }}
+        return maxsum;
+    }
+}
